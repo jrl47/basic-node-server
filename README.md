8b8190c8dd8b684065a158ec199ad65c4a5dafaf
Jacob Lettie <jrl47@users.noreply.github.com>
2020-05-18 03:28:31 -0400

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # basic-node-server
 A basic nodejs server to test out html/css/js projects nonlocally.
+
+## app.js
+This is the node.js server file. Run it with npm start.
+
+## package.json
+This is the package.json file that serves as a sort of manifest for node projects.
+For example, this is where it is specified that app.js should be run when the "npm start" command is entered.
+
+## src
+To use this server, all files to be served should be placed in the src folder.
+Basic demo html, css, and js files have been included as a demo.
---
+++ app.js
@@ -0,0 +1,39 @@
+// A minimal node.js server for static html, css, and javascript files adapted from:
+// https://developer.mozilla.org/en-US/docs/Learn/Server-side/Node_server_without_framework
+
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = process.env.PORT || 13854;
+
+http.createServer((request, response) => {
+    console.log('request ', request.url);
+
+    let filePath = '.' + request.url;
+    if (filePath == './' || filePath == './?authuser=0' /* for google's Web Preview feature */) {
+        filePath = './src/index.html';
+    } else {
+        filePath = './src' + request.url;
+    }
+
+    let extname = String(path.extname(filePath)).toLowerCase();
+    let mimeTypes = {
+        '.html': 'text/html',
+        '.js': 'text/javascript',
+        '.css': 'text/css'
+    };
+    let contentType = mimeTypes[extname];
+
+    fs.readFile(filePath, (error, content) => {
+        if (error) {
+            response.writeHead(500);
+            response.end('Error with code: ' + error.code + '.\n');
+        } else {
+            response.writeHead(200, { 'Content-Type': contentType });
+            response.end(content, 'utf-8');
+        }
+    });
+
+}).listen(PORT);
+console.log('App listening on port ' + PORT);
